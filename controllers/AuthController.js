4e66d76cf8cac278cf000695245ca0056a0917db
Mateus Lucas Joanas Marques <mateuslucas333@gmail.com>
2024-05-20 20:57:18 -0300

    implementando registerPost
    registerPost, responsável pelo post. onde vamos autenticar o registro do usuário, confirmando senha

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,13 +1,33 @@
+// importando o model User
+const User = require('../models/User')
+
+// importando biblioteca de criptografia bcrypt
+const bcrypt = require('bcryptjs')
+
 module.exports = class AuthContoller {
 
-    // login
+    // rederizar login
     static login(req, res) {
         res.render('auth/login')
     }
 
-    // registrar
+    // renderizar register
     static register(req, res) {
         res.render('auth/register')
+    }
+
+    // criar register
+    static async registerPost(req, res) {
+
+        const {name, email, password, confirmpassword} = req.body
+
+        // validação de senha
+        if(password != confirmpassword) {
+            req.flash('message', 'As senhas não conferem, tente novamente!')
+            res.render('auth/register')
+
+            return
+        }
     }
 
 }
