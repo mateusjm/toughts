8849a25933e5f962b503172c85dca9018690203e
Mateus Lucas Joanas Marques <mateuslucas333@gmail.com>
2024-05-20 15:35:17 -0300

    showToughts
    criação da função de mostrar todos os pensamentos, renderiza a view na página home

---
+++ controllers/ToughtController.js
@@ -0,0 +1,12 @@
+// importando pacotes
+const Tought = require('../models/Tought')
+const User = require('../models/User')
+
+module.exports = class ToughtController {
+
+    // mostrar pensamentos
+    static async showToughts(req, res) {
+        res.render('toughts/home')
+    }
+
+}
