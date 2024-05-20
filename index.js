8bc99e337fa59e4bd6e82ac7bb8eecb28d261b21
Mateus Lucas Joanas Marques <mateuslucas333@gmail.com>
2024-05-20 20:05:46 -0300

    importando authRoutes
    configurando a rota de autenticação no índice, sendo usado a partir da "/".

--- index.js
+++ index.js
@@ -19,6 +19,7 @@ const User = require('./models/User')
 
 // importando routes
 const toughtsRoutes = require('./routes/toughtsRoutes')
+const authRoutes = require('./routes/authRoutes')
 
 // importando controller
 const ToughtController = require('./controllers/ToughtController')
@@ -74,6 +75,7 @@ app.use((req, res, next)=> {
 
 // Routes
 app.use('/toughts', toughtsRoutes)
+app.use('/', authRoutes)
 
 // acessando todos os pensamentos pela barra
 app.get('/', ToughtController.showToughts)
