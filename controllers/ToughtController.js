038802f1aa5dc1df78b6ef1dcb948fea59c4da85
Mateus Lucas Joanas Marques <mateuslucas333@gmail.com>
2024-05-22 19:44:31 -0300

    criando createToughts
    método responsável por renderizar a página de criação de pensamentos

--- controllers/ToughtController.js
+++ controllers/ToughtController.js
@@ -4,13 +4,19 @@ const User = require('../models/User')
 
 module.exports = class ToughtController {
 
-    // mostrar pensamentos
+    // renderizar todos pensamentos
     static async showToughts(req, res) {
         res.render('toughts/home')
     }
 
+    // renderizar dashboard
     static async dashboard(req, res) {
         res.render('toughts/dashboard')
+    }
+
+    // renderizar criação de pensamentos
+    static createToughts(req, res) {
+        res.render('toughts/create')
     }
 
 }
