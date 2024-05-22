7f94003f9d50a28e53239e1892a26216b9e1f5c4
Mateus Lucas Joanas Marques <mateuslucas333@gmail.com>
2024-05-22 19:02:22 -0300

    importando helpers
    adicionando função checkAuth como parâmetro da rota dashboard

--- routes/toughtsRoutes.js
+++ routes/toughtsRoutes.js
@@ -5,7 +5,11 @@ const router = express.Router()
 // controller
 const ToughtController = require('../controllers/ToughtController')
 
+// importando função checkAuth de helpers
+const checkAuth = require('../helpers/auth').checkAuth
+
 // routes
+router.get('/dashboard', checkAuth, ToughtController.dashboard)
 router.get('/', ToughtController.showToughts)
 
 // exportando router
