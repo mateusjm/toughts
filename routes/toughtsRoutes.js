021c33d4784f31f3ee6a1fea78eb81c775e12a6c
Mateus Lucas Joanas Marques <mateuslucas333@gmail.com>
2024-05-29 11:23:44 -0300

    criando rota de atualizar pensamentos

--- routes/toughtsRoutes.js
+++ routes/toughtsRoutes.js
@@ -11,8 +11,9 @@ const checkAuth = require('../helpers/auth').checkAuth
 // routes
 router.get('/add', checkAuth, ToughtController.createToughts)
 router.post('/add', checkAuth, ToughtController.createToughtsSave)
+router.get('/edit/:id', checkAuth, ToughtController.updateTought)
 router.get('/dashboard', checkAuth, ToughtController.dashboard)
-router.post('/remove', checkAuth, ToughtController.removeToughtSave)
+router.post('/remove', checkAuth, ToughtController.removeToughtsSave)
 router.get('/', ToughtController.showToughts)
 
 // exportando router
